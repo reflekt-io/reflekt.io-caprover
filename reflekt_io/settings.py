00cebd4ec74bda9f353a2b8e4a11f5b6ce6762dd
Muhammad Athallah <mhd.athallah@gmail.com>
2024-04-22 10:58:29 +0700

    change: default db

--- reflekt_io/settings.py
+++ reflekt_io/settings.py
@@ -119,12 +119,24 @@ WSGI_APPLICATION = 'reflekt_io.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # Set database settings automatically using DATABASE_URL.
 if DB_HOST:
