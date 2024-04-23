e377ce96da0c670c7337e1502fb6f30eef8ab9d1
Muhammad Athallah <28977286+determinedguy@users.noreply.github.com>
2024-04-23 20:53:35 +0700

    huft

--- reflekt_io/settings.py
+++ reflekt_io/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import environ
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,12 +126,12 @@ WSGI_APPLICATION = 'reflekt_io.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Set database settings automatically using DATABASE_URL.
 if DB_HOST:
